5b37dd90707dc51e933253ead89e5d4d62e2370c
Matthieu <reungoat.matthieu@gmail.com>
2022-12-08 15:04:07 +0100

    Add Header & Navigation components

---
+++ src/components/Navigation.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li>ACCUEIL</li>
+    <li>A PROPOS</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+};
+</script>
+
+<style scoped>
+ul {
+  display: flex;
+  gap: 2rem;
+}
+li:hover {
+  cursor: pointer;
+}
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,31 @@
+<script setup>
+import Navigation from '../Navigation.vue';
+</script>
+
+<template>
+  <header>
+    <div>
+      <img src="../../assets/logo/logo.svg" alt="logo" />
+    </div>
+    <Navigation />
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  margin: 2rem;
+}
+img {
+  height: clamp(4.7rem, 6vw, 6.8rem);
+}
+</style>
